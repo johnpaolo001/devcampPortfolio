4d2daccb577ad51608c79ce80ecf98538fb8fd69
John Paolo Paulino <paolo716@gmail.com>
2018-07-03 21:16:29 +0800

    Implemented show functionality for portfolio module

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -26,7 +26,7 @@ class PortfoliosController < ApplicationController
 
   def update
     @portfolio_items = Portfolio.find(params[:id])
-    
+
     respond_to do |format|
       if @portfolio_items.update(params.require(:portfolio).permit(:title, :subtitle, :body))
         format.html { redirect_to portfolios_path, notice: 'The record successfully updated' }
@@ -34,6 +34,10 @@ class PortfoliosController < ApplicationController
         format.html { render :edit }
       end
     end
+  end
+
+  def show
+    @portfolio_items = Portfolio.find(params[:id])
   end
 
 
