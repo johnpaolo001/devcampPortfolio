036f0e243ede2f93443a7d565c34d907581b5232
John Paolo Paulino <paolo716@gmail.com>
2018-09-08 22:05:32 +0800

    Added validations for topics in blogs and added a topic on form elements

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -27,7 +27,7 @@ class BlogsController < ApplicationController
       @seo_keywords = @blog.body
     else
       redirect_to blogs_path, notice: "You are not authorized on this page"
-    end  
+    end
   end
 
   # GET /blogs/new
@@ -91,6 +91,6 @@ class BlogsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def blog_params
-      params.require(:blog).permit(:title, :body)
+      params.require(:blog).permit(:title, :body, :topic_id)
     end
 end
